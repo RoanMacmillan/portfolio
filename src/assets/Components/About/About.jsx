dedf85e5fbfa3596475332a4e68ee15a44dc7eb6
RoanMacmillan <hibee94@gmail.com>
2023-05-03 18:04:10 +0100

    changes

--- src/assets/Components/About/About.jsx
+++ src/assets/Components/About/About.jsx
@@ -6,15 +6,33 @@ import ContactLink from '../ContactLink/ContactLink';
 const About = () => {
   return (
     <div className={styles.about}>
-      <img className={styles.me} src={Me} alt={Me}></img>
+      <div className={styles.aboutContainer}>
+      
+
+      <div className={styles.aboutTextContainer}>
+
       <h1>
-        aspiring<br></br> <strong>frontend</strong>
+        aspiring<br></br> <strong>frontend</strong> 
         <br></br>Developer
       </h1>
       <div className={styles.headingLine}></div>
+      <h2 className={styles.titleHeading}>Roan Macmillan</h2>
+      <p className={styles.greeting}>Hi, I'm Roan Macmillan, a frontend developer based in Edinburgh. Feel free to check out my portfolio.</p>
+      
+      <a className={styles.goTo}>Go to Portfolio</a>
+      <div className={styles.linkLine}></div>
 
-      <h2>About me</h2>
-      <p>
+
+      </div>
+      <div className={styles.meWrapper}>
+      <img className={styles.me} src={Me} alt={Me}></img>
+      </div>
+      </div>
+
+
+
+      {/* <h2 className={styles.aboutMeTitle}>About me</h2>
+      <p className={styles.aboutMe}>
         Hi, I'm Roan Macmillan, a frontend developer based in
         Edinburgh. I'm passionate about creating beautiful and intuitive
         websites that provide a seamless user experience. I believe that great
@@ -22,10 +40,11 @@ const About = () => {
         and I'm always looking for new ways to improve my skills and knowledge
         in the field. In my free time, you can find me experimenting with new
         design tools and frameworks, or working on personal projects to further
-        {/* develop my frontend development skills. I'm excited to continue growing
+        develop my skills. 
+        I'm excited to continue growing
         as a developer and to contribute to the world of web design and
-        development. */}
-      </p>
+        development.
+      </p> */}
       <ContactLink />
     </div>
   );
--- src/assets/Components/PortfolioDetail/PortfolioDetail.jsx
+++ src/assets/Components/PortfolioDetail/PortfolioDetail.jsx
@@ -12,30 +12,61 @@ const PortfolioDetail = ({ portfolioData }) => {
   const detailTitleArray = item.detailTitle.split(" ");
   const detailTitle = detailTitleArray.join("<br>");
 
-  const heroStyle = {
-    backgroundImage: `url(${item.detailHero})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    height: "700px",
-  };
+  // const heroStyle = {
+  //   backgroundImage: `url(${item.detailHero})`,
+  //   backgroundSize: "cover",
+  //   backgroundPosition: "center",
+  //   height: "700px",
+  // };
 
   return (
     <div className={styles.portfolioDetail}>
-      <div className={styles.heroWrapper} style={heroStyle}>
+      <div className={styles.heroWrapper}>
         <span>{`0${item.id}-03`}</span>
         <h2 dangerouslySetInnerHTML={{ __html: detailTitle }}></h2>
         <div className={styles.line}></div>
+        <p className={styles.titleDesc}>
+          This e-commerce site is a platform for selling audio equipment,
+          complete with a functional cart and checkout system.
+        </p>
       </div>
       <div className={styles.portfolioDetailContent}>
-        <h3>About</h3>
-        <p className={styles.description}>{item.description}</p>
+        <div className={styles.staticWrapper}>
+          <img className={styles.staticImg} src={item.detailStatic1}></img>
+        </div>
+        {/* <div className={styles.backgroundWrapper}>
+        <div className={styles.backgroundContainer}> */}
+        <div className={styles.test}>
+          <div>
+            <p className={styles.about}>About</p>
+            <h3 className={styles.description}>{item.description}</h3>
+          </div>
+          <div className={styles.test2}>
+            <p className={styles.background}>{item.background}</p>
+            <div className={styles.detailLinks}>
+              <div>
+                <Link>Visit Live site</Link>
+                <div className={styles.linkLine}></div>
+              </div>
+              <div>
+                <Link>View code</Link>
+                <div className={styles.linkLine}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* </div>
+        
+        </div> */}
 
-        <img src={item.detailStatic1}></img>
-        <h3>Background</h3>
-        <p>{item.background}</p>
-        <div className={styles.detailLinks}>
-        <Link>Visit Live site</Link>
-        <Link>View code</Link>
+        <div className={styles.staticColumn}>
+          <div className={styles.staticWrapper}>
+            <img className={styles.staticImg} src={item.detailStatic2}></img>
+          </div>
+
+          <div className={styles.staticWrapper}>
+            <img className={styles.staticImg} src={item.detailStatic3}></img>
+          </div>
         </div>
       </div>
       <Navigation />
