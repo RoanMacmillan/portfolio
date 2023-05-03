413fd7f0340c99ccf61fc7d6224755c5d4eee4c0
RoanMacmillan <hibee94@gmail.com>
2023-05-03 23:40:54 +0100

    added props to reuse this component

--- src/assets/Components/ContactLink/ContactLink.jsx
+++ src/assets/Components/ContactLink/ContactLink.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
-import styles from './ContactLink.module.css'
-import { Link } from 'react-router-dom'
-const ContactLink = () => {
+import React from 'react';
+import styles from './ContactLink.module.css';
+import { Link } from 'react-router-dom';
+import arrowUp  from '../../Images/icon-arrow-up.svg'
+import useInterSectionObserver from '../../Components/userInterSectionObserver/useInterSectionObserver';
+
+
+
+const ContactLink = ({ h3Text, strongText, linkText, linkTo }) => {
+
+
+  const [scrollBtnRef, scrollBtnVisible] = useInterSectionObserver({}, true);
+  const [contactWrapperRef, contactWrapperVisible] = useInterSectionObserver({}, true);
+
+
   return (
     <div className={styles.contactLink}>
-        <div className={styles.contactLine}></div>
-      <h3>Let's<br></br><strong>Talk</strong></h3>
-      <div className={styles.cLinkContainer}>
-      <Link className={styles.contactLink}>Contact</Link>
-      <div className={styles.contactUnderline}></div>
-      </div>
-    </div>
-  )
-}
+      <div className={styles.contactLine}></div>
+      <div ref={contactWrapperRef} className={`${styles.contactWrapper} ${contactWrapperVisible ? 'fadeInLoad' : 'hidden'}`}>
 
-export default ContactLink
+      <h3>
+        {h3Text}
+        <br />
+        <strong>{strongText}</strong>
+      </h3>
+      <div className={styles.cLinkContainer}>
+        <Link to={linkTo} className={styles.contactA}>{linkText}</Link>
+        <div className={styles.contactUnderline}>
+
+          <div className={styles.loadingLine}></div>
+        </div>
+      </div>
+      </div>
+      {/* <div className={styles.scrollBtn}> */}
+
+        <div ref={scrollBtnRef} className={`${styles.scrollBtn} ${scrollBtnVisible ? 'fadeInRight' : 'hiddenRight'}`}>
+
+      <p className={styles.scrollBtnText}>Scroll to top</p>
+      <img src={arrowUp} alt={arrowUp} className={styles.arrowUp} ></img>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default ContactLink;
