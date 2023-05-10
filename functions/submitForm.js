3753d7eba0a29d79abaa8a76bd9d444421131f1d
RoanMacmillan <hibee94@gmail.com>
2023-05-10 17:09:45 +0100

    form functionality

---
+++ functions/submitForm.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async function (event, context) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+    };
+  }
+
+  const { name, email, message } = JSON.parse(event.body);
+
+  const YOUR_EMAIL = process.env.YOUR_EMAIL;
+  const YOUR_EMAIL_PASSWORD = process.env.YOUR_EMAIL_PASSWORD;
+
+  const transporter = nodemailer.createTransport({
+    service: "hotmail", // Use your email service here
+    auth: {
+      user: YOUR_EMAIL,
+      pass: YOUR_EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: YOUR_EMAIL,
+    subject: `New message from ${name}`,
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: "Message sent successfully",
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Error sending message",
+    };
+  }
+};
--- src/assets/Components/Contact/Contact.jsx
+++ src/assets/Components/Contact/Contact.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
-import styles from './Contact.module.css'
-import Mail from '../../Images/icon-mail.svg'
-import {Link} from 'react-router-dom'
-import ContactLink from '../ContactLink/ContactLink'
+import styles from "./Contact.module.css";
+import Mail from "../../Images/icon-mail.svg";
+import { Link } from "react-router-dom";
+import ContactLink from "../ContactLink/ContactLink";
 import useIntersectionObserver from "../userInterSectionObserver/useInterSectionObserver";
-
-
+import axios from "axios";
 
 const Contact = () => {
-
   const [formWrapperRef, formWrapperVisible] = useIntersectionObserver();
 
   const [name, setName] = useState("");
@@ -39,13 +37,13 @@ const Contact = () => {
     return "";
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
+  
     const nameError = validateName(name);
     const emailError = validateEmail(email);
     const messageError = validateMessage(message);
-
+  
     if (nameError || emailError || messageError) {
       setErrors({
         name: nameError,
@@ -54,134 +52,151 @@ const Contact = () => {
       });
       return;
     }
-
+  
     const data = {
       name,
       email,
       message,
     };
-
-    console.log(data);
-    setErrors({});
-    setFormSubmitted(true);
+  
+    try {
+      const response = await axios.post("/.netlify/functions/submitForm", data);
+  
+      if (response.status === 200) {
+        console.log("Message sent successfully");
+        setErrors({});
+        setFormSubmitted(true);
+      } else {
+        console.error("Error sending message");
+        setErrors({ form: "Error sending message. Please try again later." });
+      }
+    } catch (error) {
+      console.error("Error sending message", error);
+      setErrors({ form: "Error sending message. Please try again later." });
+    }
   };
-
-
 
   return (
     <div className={styles.contact}>
-      <h1>Let's<br></br> <strong>Talk</strong></h1>
+      <h1>
+        Let's<br></br> <strong>Talk</strong>
+      </h1>
       <div className={styles.headingLine}></div>
 
       <div className={styles.contactContainer}>
-
-      <div className={styles.contactText}>
-      <h2>Contact form</h2>
-      <p className={styles.contactP}>Got a project in mind? Find my contact info below or feel free to fill out the form below with your questions.</p>
-      </div>
-      <form onSubmit={handleSubmit} noValidate>
-        <div ref={formWrapperRef} className={`${styles.formWrapper} ${formWrapperVisible ? 'fadeInLoad' : 'hidden' }`}>
-        <div className={styles.formTop}>
-      <div className={styles.formGroup}>
-        <label htmlFor="name">Name{errors.name && <div className={styles.error}>{errors.name}</div>}</label>
-        
-
-        <input
-          className={`${styles.formInput} ${
-            errors.name
-              ? styles.inputError
-              : validateName(name) === ""
-              ? styles.validInput
-              : ""
-          }`}
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Din Djarin"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="email">Email Address {errors.email && <div className={styles.error}>{errors.email}</div>}</label>
-        
-
-        <input
-          className={`${styles.formInput} ${
-            errors.email
-              ? styles.inputError
-              : validateEmail(email) === ""
-              ? styles.validInput
-              : ""
-          }`}
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
-      </div>
-
-      <div className={styles.formGroup}>
-        <label htmlFor="message">Message {errors.message && <div className={styles.error}>{errors.message}</div>}</label>
-        
-
-        <textarea
-          className={`${styles.formInput} ${
-            errors.message
-              ? styles.inputError
-              : message.trim() !== ""
-              ? styles.validInput
-              : ""
-          }`}
-          id="message"
-          name="message"
-          rows="4"
-          placeholder="How can I help?"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-        ></textarea>
-      </div>
-      <div className={styles.tyContainer}>
-        <button className={styles.submitBtn} type="submit">
-          Send Now
-        </button>
-        {/* {formSubmitted && (
-          <p className={styles.thankyouMessage}>
-            Thank you for contacting me! I'll get back to you as soon as
-            possible.
+        <div className={styles.contactText}>
+          <h2>Contact form</h2>
+          <p className={styles.contactP}>
+            Got a project in mind? Find my contact info below or feel free to
+            fill out the form below with your questions.
           </p>
-        )} */}
-      </div>
-      </div>
-    </form>
-    <div className={styles.contactDetails}>
+        </div>
+        <form onSubmit={handleSubmit} noValidate>
+          <div
+            ref={formWrapperRef}
+            className={`${styles.formWrapper} ${
+              formWrapperVisible ? "fadeInLoad" : "hidden"
+            }`}
+          >
+            <div className={styles.formTop}>
+              <div className={styles.formGroup}>
+                <label htmlFor="name">
+                  Name
+                  {errors.name && (
+                    <div className={styles.error}>{errors.name}</div>
+                  )}
+                </label>
 
-      <img src={Mail} alt={Mail} className={styles.mailIcon}></img> 
+                <input
+                  className={`${styles.formInput} ${
+                    errors.name
+                      ? styles.inputError
+                      : validateName(name) === ""
+                      ? styles.validInput
+                      : ""
+                  }`}
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Din Djarin"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="email">
+                  Email Address{" "}
+                  {errors.email && (
+                    <div className={styles.error}>{errors.email}</div>
+                  )}
+                </label>
+
+                <input
+                  className={`${styles.formInput} ${
+                    errors.email
+                      ? styles.inputError
+                      : validateEmail(email) === ""
+                      ? styles.validInput
+                      : ""
+                  }`}
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="message">
+                Message{" "}
+                {errors.message && (
+                  <div className={styles.error}>{errors.message}</div>
+                )}
+              </label>
+
+              <textarea
+                className={`${styles.formInput} ${
+                  errors.message
+                    ? styles.inputError
+                    : message.trim() !== ""
+                    ? styles.validInput
+                    : ""
+                }`}
+                id="message"
+                name="message"
+                rows="4"
+                placeholder="How can I help?"
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+              ></textarea>
+            </div>
+            <div className={styles.tyContainer}>
+              <button className={styles.submitBtn} type="submit">
+                Send Now
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className={styles.contactDetails}>
+          <img src={Mail} alt={Mail} className={styles.mailIcon}></img>
 
           <h4>roanmacmillan@hotmail.com</h4>
 
           <p>Send me a message and I will reach out as soon as possible.</p>
+        </div>
+      </div>
 
+      <ContactLink
+        h3Text="View"
+        strongText="Portfolio"
+        linkText="Portfolio"
+        linkTo="/"
+      />
     </div>
-    </div>
+  );
+};
 
-    
-
-    {/* <div className={styles.contactLink}>
-        <div className={styles.contactLine}></div>
-      <h3>See<br></br><strong>Portfolio</strong></h3>
-      <Link to='/' className={styles.contactLink}>Portfolio</Link>
-      <div className={styles.contactUnderline}></div>
-    </div> */}
-
-    <ContactLink h3Text="View" strongText="Portfolio" linkText="Portfolio"
-        linkTo="/" />
-      
-    </div>
-  )
-}
-
-export default Contact
+export default Contact;
