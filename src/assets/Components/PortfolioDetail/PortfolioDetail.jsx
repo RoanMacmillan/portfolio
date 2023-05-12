830ad1d02c896ab4a2a2aba5b3b08f9ca51e27d0
RoanMacmillan <hibee94@gmail.com>
2023-05-12 14:13:00 +0100

    fixes

--- src/assets/Components/PortfolioDetail/PortfolioDetail.jsx
+++ src/assets/Components/PortfolioDetail/PortfolioDetail.jsx
@@ -8,7 +8,6 @@ import LinkBtn from "../Link/LinkBtn";
 const PortfolioDetail = ({ portfolioData }) => {
   const { id } = useParams();
   const item = portfolioData.find((item) => item.id === id);
-  
 
   const [staticWrapperRef, staticWrapperVisible] = useIntersectionObserver();
   const [staticWrapperRef2, staticWrapperVisible2] = useIntersectionObserver();
@@ -24,9 +23,7 @@ const PortfolioDetail = ({ portfolioData }) => {
         <span>{`0${item.id}-03`}</span>
         <h2>{item.detailTitle}</h2>
         <div className={styles.line}></div>
-        <p className={styles.titleDesc}>
-          {item.description}
-        </p>
+        <p className={styles.titleDesc}>{item.description}</p>
       </div>
       <div className={styles.portfolioDetailContent}>
         <div
@@ -62,6 +59,35 @@ const PortfolioDetail = ({ portfolioData }) => {
             <img className={styles.staticImg} src={item.detailStatic3}></img>
           </div>
         </div>
+
+        {/* <div className={styles.backgroundCont}>
+          <h3 className={`${styles.description} ${styles.backgroundTitle}`}>
+            Project background
+          </h3>
+
+          <p className={styles.backgroundText}>
+            We visited interesting interior designs in the modern plebiscite.
+            Please check and view the collection. Creative projects are gaining
+            new popularity as they surprise recipients with new possibilities.
+            We pay attention to the user experience when viewing projects to
+            create an advantage.
+          </p>
+          <div className={styles.linkContainer}>
+            <LinkBtn linkToPage={item.demoURL} linkContent="View Demo" />
+            <LinkBtn linkToPage={item.codeURL} linkContent="View Code" />
+          </div>
+
+        </div>
+
+
+
+        <div className={styles.bigImgContainer}>
+
+<img src={item.detailStatic2}></img>
+
+</div> */}
+
+
       </div>
       <ContactLink
         h3Text="See"
